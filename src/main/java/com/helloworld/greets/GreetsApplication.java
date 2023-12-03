06e951f8ed502a9caa5d50cc7ec5af12f786ffea
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-12-04 05:47:34 +0900

    first-push

---
+++ src/main/java/com/helloworld/greets/GreetsApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.greets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworld/greets/GreetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.greets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
